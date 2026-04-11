e18d722225a700597e58eb706a0c086b3cbc19d4
Jack Harding <jackharding321@gmail.com>
2026-04-11 13:23:35 -0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # this is a common problem that cane be solve with the double pointer
+        # A left pointer is assigned to the buy at the 0th element
+        # A right pointer is assigned to the sell at 1st element
+        # this will have a time complexity of O(n) while the brute force will have O(n²)
+        l = 0
+        r = 1
+        max_prof = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                prof = prices[r] - prices[l]
+                max_prof = max(prof, max_prof)
+            else:
+                # shift the left pointer (buy) all the way to the index with the cheapest buy
+                l = r
+            r += 1
+        return max_prof
+
