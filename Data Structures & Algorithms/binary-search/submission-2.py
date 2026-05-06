508072e66f84d4f22803da578d27da0cff458507
Jack Harding <jackharding321@gmail.com>
2026-05-06 07:38:20 -0700

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Must be sorted to use binary search
+        # Find the middle and compare to target to choose traversal direction
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            # Number found
+            if target == nums[mid]:
+                return mid
+
+            # Ignore left
+            if target > nums[mid]:
+                low = mid + 1
+                continue
+                
+            # Ignore right
+            high = mid - 1
+        return -1
+            
