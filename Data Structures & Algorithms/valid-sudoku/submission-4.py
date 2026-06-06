da34ff3f6b8d0e6afccd15797af7318bc0f56595
Jack Harding <jackharding321@gmail.com>
2026-06-06 09:17:46 -0700

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = []
+        cube_ctr = 0
+        for i in range(9):
+            row = board[i]
+            # check row is unique
+            unique_chars = [r for r in row if r != "."]
+            if len(set(unique_chars)) != len(unique_chars):
+                return False
+            # check col is unique
+            col = [board[j][i] for j in range(9) if board[j][i] != "."]
+            if len(set(col)) != len(col):
+                return False
+
+            # check all squares in the grid
+            row_start = (i // 3) * 3
+            col_start = (i % 3) * 3
+            square = [board[r][c] for r in range(row_start, row_start + 3) for c in range(col_start, col_start + 3) if board[r][c] != "."]
+            if len(square) != len(set(square)):
+                return False
+
+        return True
