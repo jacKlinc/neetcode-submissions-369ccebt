80a64f696285cedf4a584476aa8ad80a8c3c20be
Jack Harding <jackharding321@gmail.com>
2026-07-17 16:35:04 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,24 @@
+class MinStack:
+    def __init__(self):
+        self.vals = []
+
+    def push(self, val: int) -> None:
+        self.vals.append(val)
+
+    def pop(self) -> None:
+        self.vals.pop()
+
+    def top(self) -> int:
+        return self.vals[-1]
+
+    def getMin(self) -> int:
+        l = []
+        min_val = self.vals[-1]
+        # find min
+        while len(self.vals):
+            min_val = min(min_val, self.vals[-1])
+            l.append(self.vals.pop())
+        # Repopualte stack
+        while len(l):
+            self.vals.append(l.pop())
+        return min_val
