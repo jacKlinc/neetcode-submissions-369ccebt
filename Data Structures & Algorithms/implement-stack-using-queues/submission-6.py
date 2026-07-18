ba787f6c76c4c2aed8ffabda4cbd38917e569db0
Jack Harding <jackharding321@gmail.com>
2026-07-18 12:26:54 -0700

    Add: implement-stack-using-queues - submission-6

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-6.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+class MyStack:
+    def __init__(self):
+        self.vals = deque()
+
+    def push(self, x: int) -> None:
+        self.vals.append(x)
+
+    def pop(self) -> int:
+        for _ in range(len(self.vals) - 1):
+            self.push(self.vals.popleft())
+        # return the last value
+        return self.vals.popleft()
+
+    def top(self) -> int:
+        return self.vals[-1]
+
+    def empty(self) -> bool:
+        return not self.vals
