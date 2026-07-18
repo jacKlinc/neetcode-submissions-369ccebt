cae7f1ad75007ae24bc2e613abf6db09aefd13f9
Jack Harding <jackharding321@gmail.com>
2026-07-18 10:37:48 -0700

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        par_map = {"]": "[", "}": "{", ")": "("}
+        stack = []
+        
+        for c in s:
+            if c in par_map:
+                if stack and stack[-1] == par_map[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return not stack
