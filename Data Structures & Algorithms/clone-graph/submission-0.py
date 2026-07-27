74168b32a677b9c6a48d4cd961c765d4b00eae70
Jack Harding <jackharding321@gmail.com>
2026-07-27 13:23:16 -0700

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,31 @@
+"""
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+class Solution:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        if not node:
+            return
+
+        # map old to new values
+        old_new = {}
+        # for: 1 -> 2 -> 3
+        # clone 1 in map
+        # clone 2 in map, save 1, 3 as neighbor
+        # use Node 3 (already exists) in map, save 2 as neighbor but it already exists no need to create a new Node
+
+        def dfs(node):
+            if node in old_new:
+                return old_new[node]
+
+            clone = Node(node.val)
+            old_new[node] = clone
+            clone.neighbors = [dfs(n) for n in node.neighbors]
+
+            return clone
+
+        return dfs(node)
