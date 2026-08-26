dfc6463d25657db141c04b98a60a1ae28c4c132a
Jack Harding <jackharding321@gmail.com>
2026-08-26 09:10:26 -0700

    Add: validate-parentheses - submission-17

---
+++ Data Structures & Algorithms/validate-parentheses/submission-17.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # if the length is odd it must be false
+        # if len(s) % 2 != 0:
+        #     return False
+
+        par_map = {"]": "[", ")": "(", "}": "{"}
+        stack = []
+        # Pop off the stack until empty
+        for c in s:
+            print(stack)
+            # Closing bracket
+            if c in par_map:
+                # If not empty and top matches the opening bracket
+                if stack and stack[-1] == par_map[c]:
+                    stack.pop()  # pop and move on
+                    continue
+                return False
+            # Push opening bracket onto stack
+            stack.append(c)
+
+        return not stack
