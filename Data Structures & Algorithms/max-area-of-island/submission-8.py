94a54e85f820295c820b12df6bd317026f9ec697
Jack Harding <jackharding321@gmail.com>
2026-09-03 13:01:57 -0700

    Add: max-area-of-island - submission-8

---
+++ Data Structures & Algorithms/max-area-of-island/submission-8.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        # the edge is water
+        # The example actually has three islands but we want the biggest one
+        ROWS, COLS = len(grid), len(grid[0])
+        parents = list(range(n := ROWS * COLS))
+        rank = [1] * n
+
+        def find(x):
+            if parents[x] != x:
+                parents[x] = find(parents[x])
+            return parents[x]
+
+        def union(x, y):
+            px, py = find(x), find(y)
+            if px == py:
+                return False
+
+            if rank[px] > rank[py]:
+                rank[px] += rank[py]
+                parents[py] = px
+            else:
+                rank[py] += rank[px]
+                parents[px] = py
+
+            return False
+
+        max_area = 0
+        index = lambda r, c: r * COLS + c
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    continue
+                # only right and down needed for this 
+                for dr, dc in [(1, 0), (0, 1)]:
+                    i, j = r + dr, c + dc
+                    if i not in range(ROWS) or j not in range(COLS) or grid[i][j] == 0:
+                        continue
+                    union(index(r, c), index(i, j))
+                
+                # Find size of the connected component
+                area = rank[find(index(r, c))]
+                max_area = max(max_area, area)
+
+        return max_area
