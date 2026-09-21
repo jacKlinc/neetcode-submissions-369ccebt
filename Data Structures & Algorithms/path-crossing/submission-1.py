9140612e4ec99266cc668d3ec02bbcf48750bbde
Jack Harding <jackharding321@gmail.com>
2026-09-21 10:45:05 -0700

    Add: path-crossing - submission-1

---
+++ Data Structures & Algorithms/path-crossing/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        dir_map = {
+            "N": (0, 1),
+            "S": (0, -1),
+            "E": (1, 0),
+            "W": (-1, 0),
+        }
+        visit = set()
+        x, y = 0, 0
+
+        for c in path:
+            visit.add((x, y))
+            dx, dy = dir_map[c]
+            x, y = x + dx, y + dy
+            if (x, y) in visit:
+                return True
+
+        return False
